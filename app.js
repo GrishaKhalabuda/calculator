63951bae9a0d1d74d9a16bf3173a1a2f2226c435
GrishaKhalabuda <halabuda17@gmail.com>
2024-02-07 15:24:58 +0200

    addBtn+-

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 
 const submitBtn = document.getElementById("submit");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
 submitBtn.onclick = function () {
-  const sum = Number(input1.value) + Number(input2.value);
-  resultElement.textContent = sum;
+  if (action == "+") {
+    const sum = Number(input1.value) + Number(input2.value);
+    resultElement.textContent = sum;
+  } else if (action == "-") {
+    const sum = Number(input1.value) - Number(input2.value);
+    resultElement.textContent = sum;
+  }
+};
+plusBtn.onclick = function () {
+  action = "+";
+};
+minusBtn.onclick = function () {
+  action = "-";
 };
